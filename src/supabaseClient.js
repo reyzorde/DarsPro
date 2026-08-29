0059300972c5419f77ada34535b5e84131d50807
reyzorde <sirojidding999@gmail.com>
2026-08-29 13:54:37 +0300

    not env

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = "https://ovhvramqwdnuhlyzyayz.supabase.co"
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im92aHZyYW1xd2RudWhseXp5YXl6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc5MDAyMDMsImV4cCI6MjEwMzQ3NjIwM30.C0YC7L7Agysc0HDjFGn7RuLM_1I5MHRxM_fGUEmgSgY"
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
